47608cfa16fa3327d11282b5ae05e2534071b612
Nick Lamarca <nicklamarca64@gmail.com>
2024-08-18 00:38:28 -0500

    Implemented Publish For Transfer Banking Microservice

--- MicroRabbit.Banking.Api/Controllers/BankingController.cs
+++ MicroRabbit.Banking.Api/Controllers/BankingController.cs
@@ -25,7 +25,7 @@ namespace MicroRabbit.Banking.Api.Controllers
         [HttpPost]
         public IActionResult Post([FromBody] AccountTransfer accountTransfer)
         {
-            //_accountService.Transfer(accountTransfer);
+            _accountService.Transfer(accountTransfer);
             return Ok(accountTransfer);
         }
    
--- MicroRabbit.Banking.Domain/CommandHandlers/TransferCommandHandler.cs
+++ MicroRabbit.Banking.Domain/CommandHandlers/TransferCommandHandler.cs
@@ -1,5 +1,6 @@
 ﻿using MediatR;
 using MicroRabbit.Banking.Domain.Commands;
+using MicroRabbit.Banking.Domain.Events;
 using MicroRabbit.Domain.Core.Bus;
 using System;
 using System.Collections.Generic;
@@ -20,6 +21,7 @@ namespace MicroRabbit.Banking.Domain.CommandHandlers
         public Task<bool> Handle(CreateTransferCommand request, CancellationToken cancellationToken)
         {
            //publish event to RabbitMQ
+           _bus.Publish(new TransferCreatedEvent(request.From, request.To, request.Amount));
 
            return Task.FromResult(true);
         }

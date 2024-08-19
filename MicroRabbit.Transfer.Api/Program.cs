0fb9148c7644e4f4b1102fee590d42524259756a
Nick Lamarca <nicklamarca64@gmail.com>
2024-08-19 18:36:05 -0500

    Configure Transfer Startup And migration Scripts

--- MicroRabbit.Transfer.Api/Program.cs
+++ MicroRabbit.Transfer.Api/Program.cs
@@ -1,3 +1,7 @@
+using MicroRabbit.Infra.IoC;
+using MicroRabbit.Transfer.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
 namespace MicroRabbit.Transfer.Api
 {
     public class Program
@@ -7,14 +11,30 @@ namespace MicroRabbit.Transfer.Api
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            builder.Services.AddDbContext<TransferDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("TransferDbConnection")));
 
             builder.Services.AddControllers();
+
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new() { Title = "Transfer Microservice", Version = "v1" });
+            });
+
+            // Add MediatR FIX THIS
+            builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(Program).Assembly));
+
+
+            DependencyContianer.RegisterServices(builder.Services);
 
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
 
             app.UseHttpsRedirection();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Transfer Microservice V1"));
 
             app.UseAuthorization();
 
---
+++ MicroRabbit.Transfer.Data/Context/TransferDbContextFactory.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+using MicroRabbit.Transfer.Data.Context;
+
+namespace MicroRabbit.Banking.Data.Context
+{
+    public class TransferDbContextFactory : IDesignTimeDbContextFactory<TransferDbContext>
+    {
+        public TransferDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<TransferDbContext>();
+
+            // Specify the connection string used for design-time operations.
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=TransferDB;Trusted_Connection=True;MultipleActiveResultSets=true");
+
+            return new TransferDbContext(optionsBuilder.Options);
+        }
+    }
+}

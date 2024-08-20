4070029e3915817e8dad82c343a29f640a73e8a4
Nick Lamarca <nicklamarca64@gmail.com>
2024-08-19 22:42:17 -0500

    Setup MVC Presentation Layer And Make A Call To Banking Microservice

--- MicroRabbit.Banking.Application/Models/AccountTransfer.cs
+++ MicroRabbit.Banking.Application/Models/AccountTransfer.cs
@@ -8,9 +8,8 @@ namespace MicroRabbit.Banking.Application.Models
 {
     public class AccountTransfer
     {
-        public int ToAccount { get; set; }
         public int FromAccount { get; set; }
-
+        public int ToAccount { get; set; }
         public decimal TransferAmount { get; set; }
     }
 }
---
+++ MicroRabbit.MVC/Services/TransferService.cs
@@ -0,0 +1,26 @@
+﻿using MicroRabbit.MVC.Models.DTO;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace MicroRabbit.MVC.Services
+{
+    public class TransferService : ITransferService
+    {
+        private readonly HttpClient _apiClient;
+
+        public TransferService(HttpClient apiClient)
+        {
+           _apiClient = apiClient;
+        }
+        public async Task Transfer(TransferDto transferDto)
+        {
+           var uri = "https://localhost:7056/api/banking";
+           var transferContent = new StringContent(JsonConvert.SerializeObject(transferDto), 
+                                                    Encoding.UTF8, "application/json");
+
+           var response = await _apiClient.PostAsync(uri, transferContent);
+           response.EnsureSuccessStatusCode();
+        }
+
+    }
+}
